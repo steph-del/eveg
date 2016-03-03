5634ae42af1fae65e9c078148f59f2749a8b4dde
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-03 21:51:40 +0100

    Add fullName to the registration form type
    (extends FOS RegistrationType)

---
+++ src/eveg/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,25 @@
+<?php
+// eveg/UserBundle/Form/RegistrationType.php
+
+namespace eveg\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('fullName');
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+}
