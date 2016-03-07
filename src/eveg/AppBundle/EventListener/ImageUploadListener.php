b38b1f91075e29753aa91cf31e8338e828ae9228
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-07 21:59:38 +0100

    Listener on photo upload : resize and thumbnails

---
+++ src/eveg/AppBundle/EventListener/ImageUploadListener.php
@@ -0,0 +1,52 @@
+<?php
+// src/eveg/AppBundle/EventListener/ImageUploadListener.php
+namespace eveg\AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Vich\UploaderBundle\Event\Event;
+
+class ImageUploadListener
+{
+	protected $uploadHelper;
+	protected $imagineDataManager;
+	protected $imagineFilterManager;
+	protected $imagineCacheManager;
+
+	public function __construct($uploader_helper, $data_manager, $filter_manager, $cache_manager)
+        {
+            $this->uploadHelper = $uploader_helper;
+            $this->imagineDataManager = $data_manager;
+            $this->imagineFilterManager = $filter_manager;
+            $this->imagineCacheManager = $cache_manager;
+        }
+
+    public function onPostUpload(Event $event)
+    {
+    	// get the uploaded file
+        $uploadedFile = $event->getObject();
+
+        // get the path of the uploaded file
+		$path = $this->uploadHelper->asset($uploadedFile, 'imageFile');
+
+		// retrieve the file as a BinaryInterface
+		$image = $this->imagineDataManager->find('thumb', $path);
+
+    	// Resize
+    		// applyFilter returns BinaryInterface
+			$resized = $this->imagineFilterManager->applyFilter($image, 'resized');
+
+			// store the resized image
+			$this->imagineCacheManager->store($resized, $path, 'resized');
+
+    	// Thumb generation
+			// applyFilter returns BinaryInterface
+			$thumb = $this->imagineFilterManager->applyFilter($image, 'thumb');
+
+			// store the thumb
+			$this->imagineCacheManager->store($thumb, $path, 'thumb');
+
+
+        
+    }
+}
