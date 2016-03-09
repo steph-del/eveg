73b7cc1eaa9972a4694ef6a9d0d8e9424feb69dc
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-10 00:08:27 +0100

    SyntaxonNote (SNote) entity + repo

---
+++ src/eveg/AppBundle/Entity/SyntaxonNote.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace eveg\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SyntaxonNote
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="eveg\AppBundle\Entity\SyntaxonNoteRepository")
+ */
+class SyntaxonNote
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="user", type="object")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text")
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \stdClass $user
+     *
+     * @return SyntaxonNote
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \stdClass
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     *
+     * @return SyntaxonNote
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
+
