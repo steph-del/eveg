d0bfe45876ebab9ec4cf114064bebd1d8c5f4084
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-14 01:22:29 +0100

    $syntaxon according to user's rights new service

---
+++ src/eveg/AppBundle/Utils/Repository/getSyntaxonAccordingUserRights.php
@@ -0,0 +1,57 @@
+<?php
+// src/eveg/AppBundle/Utils/Repository/getSyntaxonAccordingUserRights.php
+
+namespace eveg\AppBundle\Utils\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Get the good repository according to user's rights (images, files and httpLinks)
+ *
+ */
+class getSyntaxonAccordingUserRights
+{
+
+	private $scRepository;
+	private $securityAuthorisation;
+	private $securityToken;
+
+	public function __construct(EntityRepository $scRepository, $securityAuthorisation, $securityToken)
+	{
+		$this->scRepository = $scRepository;
+		$this->securityAuthorisation = $securityAuthorisation;
+		$this->securityToken = $securityToken;
+	}
+
+	/**
+	 * @param integer $id SyntaxonCore id
+ 	 * @return RepositoryMethod
+ 	 */
+	public function getSyntaxon($id)
+	{
+		// Retrieve syntaxon according to user's rights
+		// 		User is authenticated anonymously (= not logged in)
+		// 		only retrieves the syntaxon with public data
+
+		$currentUser = $this->securityToken->getToken()->getUser();
+		$repo = $this->scRepository;
+
+		// User is logged in
+		if($this->securityAuthorisation->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+		{
+			// User belongs to circle group
+			if($this->securityAuthorisation->isGranted('ROLE_CIRCLE'))
+			{
+				$syntaxon = $repo->findByIdPublicPrivateCircleData($id, $currentUser);
+			// User do not belongs to circle group but it can own private data
+			} else {
+				$syntaxon = $repo->findByIdPublicPrivateData($id, $currentUser);
+			}
+		// User is not logged in
+		} else {
+			$syntaxon = $repo->findByIdPublicData($id);
+		}
+
+		return $syntaxon;
+	}
+}
