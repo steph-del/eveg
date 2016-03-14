b5d6899eb5a9d06ec0dcaa753ca841f5419c0b8a
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-15 00:32:09 +0100

    baseflor repository functions

--- src/eveg/AppBundle/Entity/BaseflorRepository.php
+++ src/eveg/AppBundle/Entity/BaseflorRepository.php
@@ -10,4 +10,37 @@ namespace eveg\AppBundle\Entity;
  */
 class BaseflorRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function findByCatminatCode($catminatCode)
+	{
+		$qb = $this->createQueryBuilder('b');
+
+		// specify the fields to fetch (unselected fields will have a null value)
+		$qb->select("b.bdnffTaxinId, b.bdnffNomenId, b.catminatCode, b.scientificName")
+			->where('b.catminatCode = :catminatCode')
+			->setParameter('catminatCode', $catminatCode)
+			->orderBy('b.scientificName', 'ASC');
+			;
+
+		return $qb->getQuery()->getResult();
+	}
+
+	public function findEcologicalAverageByCatminatCode($catminatCode)
+	{
+		$qb = $this->createQueryBuilder('b');
+
+		$qb->select("AVG(b.pjLight) pjLightAvg, 
+					 AVG(b.pjTemperature) pjTemperatureAvg, 
+					 AVG(b.pjContinentality) pjContinentalityAvg,
+					 AVG(b.pjAtmosphericHumidity) pjAtmosphericHumidityAvg, 
+					 AVG(b.pjSoilHumidity) pjSoilHumidityAvg, 
+					 AVG(b.pjSoilPh) pjSoilPhAvg,
+					 AVG(b.pjTrophicLevel) pjTrophicLevelAvg, 
+					 AVG(b.pjSalinity) pjSalinityAvg, 
+					 AVG(b.pjTexture) pjTextureAvg, 
+					 AVG(b.pjOrganicMaterial) pjOrganicMaterialAvg")
+			->where('b.catminatCode = :catminatCode')
+			->setParameter('catminatCode', $catminatCode)
+			;
+		return $qb->getQuery()->getResult();
+	}
 }
