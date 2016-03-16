c6f1abe4e14c075d1e903414043b901bcf45c06d
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-16 22:56:51 +0100

    new service : Wanted for compilation :)

---
+++ src/eveg/AppBundle/Utils/Wanted/wantedForCompilation.php
@@ -0,0 +1,75 @@
+<?php
+// src/eveg/appBundle/Utils/Wanted/wantedForCompilation.php
+
+namespace eveg\AppBundle\Utils\Wanted;
+
+/**
+ * Utility to recover pdf files to be compiled
+ *
+ */
+class wantedForCompilation
+{
+	private $sFileRepo;
+
+	public function __construct($sFileRepo)
+	{
+		$this->sFileRepo = $sFileRepo;
+	}
+
+	/**
+	 * Get the list of pdf documents concerning a diagnosis WHILE none excel file exists for the same diagnosis
+	 *
+	 * @param integer $limitResults limit the number of results
+	 */
+	public function getList($limitResults = null)
+	{
+		$spreadsheets = $this->sFileRepo->getPublicSpreadsheets($limitResults);
+		$pdfs = $this->sFileRepo->getPublicPdfs($limitResults);
+
+		$pdfsAlone = array();
+
+		foreach ($pdfs as $key => $pdf) {
+			$flag = false;
+
+			foreach ($spreadsheets as $key => $spreadsheet) {
+				if($pdf->getDiagnosisOf() == $spreadsheet->getDiagnosisOf()) {
+					$flag = true;
+				}
+			}
+
+			if($flag === false) {
+				array_push($pdfsAlone, $pdf);
+			}
+		}
+
+		return $pdfsAlone;
+	}
+
+	/**
+	 * Get the number of pdf documents concerning a diagnosis WHILE none excel file exists for the same diagnosis
+	 *
+	 */
+	public function howMany()
+	{
+		$spreadsheets = $this->sFileRepo->getPublicSpreadsheetsWithoutSCoreRelation();
+		$pdfs = $this->sFileRepo->getPublicPdfs();
+
+		$counter = 0;
+
+		foreach ($pdfs as $key => $pdf) {
+			$flag = false;
+
+			foreach ($spreadsheets as $key => $spreadsheet) {
+				if($pdf->getDiagnosisOf() == $spreadsheet->getDiagnosisOf()) {
+					$flag = true;
+				}
+			}
+
+			if($flag === false) {
+				$counter++;
+			}
+		}
+
+		return $counter;
+	}
+}
