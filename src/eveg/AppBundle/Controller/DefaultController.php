a1527997664eb98bd81f7c6cc01587aab21a9ff9
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-20 22:32:49 +0100

    Maps cache images : export pdf integration

--- src/eveg/AppBundle/Controller/DefaultController.php
+++ src/eveg/AppBundle/Controller/DefaultController.php
@@ -83,7 +83,7 @@ class DefaultController extends Controller
 		    200,
 		    array(
 		        'Content-Type'          => 'application/pdf',
-		        'Content-Disposition'   => 'attachment; filename="file.pdf"'
+		        'Content-Disposition'   => 'attachment; filename='.$id.'.pdf'
 		    )
 		);
 
