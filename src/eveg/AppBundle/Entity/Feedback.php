8a21888caf33fcc38448d8af92b453bc6408df63
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-20 23:01:45 +0100

    Feedback : entity + repo

---
+++ src/eveg/AppBundle/Entity/Feedback.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace eveg\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="eveg\AppBundle\Entity\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="about", type="string", length=255)
+     * @Assert\Choice(choices = {"other", "syntaxon", "depFrRepartition", "europeRepartition"})
+     */
+    private $about;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     * @Assert\Choice(choices = {"data", "interface"})
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="syntaxon", type="string", length=512)
+     */
+    private $syntaxon;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\ManyToOne(targetEntity="eveg\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_id", nullable=false)
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set about
+     *
+     * @param string $about
+     *
+     * @return Feedback
+     */
+    public function setAbout($about)
+    {
+        $this->about = $about;
+
+        return $this;
+    }
+
+    /**
+     * Get about
+     *
+     * @return string
+     */
+    public function getAbout()
+    {
+        return $this->about;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Feedback
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set syntaxon
+     *
+     * @param string $syntaxon
+     *
+     * @return Feedback
+     */
+    public function setSyntaxon($syntaxon)
+    {
+        $this->syntaxon = $syntaxon;
+
+        return $this;
+    }
+
+    /**
+     * Get syntaxon
+     *
+     * @return string
+     */
+    public function getSyntaxon()
+    {
+        return $this->syntaxon;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Feedback
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \stdClass $user
+     *
+     * @return Feedback
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \stdClass
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Feedback
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
+
