f16dcee93321fa0399a97e37f23d6706cfb50d47
steph-del <dev.stephane.delplanque@gmail.com>
2016-03-24 10:58:43 +0100

    update compare controller

--- src/eveg/AppBundle/Controller/CompareController.php
+++ src/eveg/AppBundle/Controller/CompareController.php
@@ -4,6 +4,8 @@
 namespace eveg\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -38,7 +40,7 @@ class CompareController extends Controller
 		return new JsonResponse($session->get('eveg_compare'));
 	}
 
-	public function addItemAction($id, $value)
+	public function addItemAction()
 	{
 		// Session already started ?
 		if(!$this->container->get('session')->isStarted()) {
@@ -55,7 +57,9 @@ class CompareController extends Controller
 		if($nbItems >= 2) {
 			return new JsonResponse("Error, you already have ".$nbItems." items for comparison, that is enough !");
 		} else {
-			$compareArray[$id] = $value;
+
+			$currentId = $session->get('idReferer');
+			$compareArray[$currentId] = $session->get('syntaxonNameReferer');
 
 			$session->set('eveg_compare', $compareArray);
 
@@ -63,7 +67,7 @@ class CompareController extends Controller
 		}
 	}
 
-	public function removeItemAction($id)
+	public function removeItemAction(Request $request)
 	{
 		// Session already started ?
 		if(!$this->container->get('session')->isStarted()) {
@@ -72,6 +76,8 @@ class CompareController extends Controller
 		} else {
 			$session = $this->container->get('session');
 		}
+
+		$id = $request->get('id');
 
 		$compareArray = $session->get('eveg_compare');
 
@@ -102,5 +108,48 @@ class CompareController extends Controller
 		$session->set('eveg_compare', '');
 
 		return new JsonResponse($session->get('eveg_compare'));
+	}
+
+	public function compareAction()
+	{
+		// Session already started ?
+		if(!$this->container->get('session')->isStarted()) {
+			$session = new Session();
+			$session->start();
+		} else {
+			$session = $this->container->get('session');
+		}
+
+		$compareArray = $session->get('eveg_compare');
+		$nbItems = count($compareArray);
+		$ids = array();
+
+		if($nbItems == 2) {
+			
+			foreach ($compareArray as $key => $value) {
+				array_push($ids, $key);
+			}
+
+			// Retrieve syntaxon according to user's rights
+			$findGoodRepo = $this->get('eveg_app.get_syntaxon_according_user');
+			// Get syntaxon1
+			$syntaxon1 = $findGoodRepo->getSyntaxon($ids[0], $depFrFilter = null, $ueFilter = null);
+			// Get syntaxon2
+			$syntaxon2 = $findGoodRepo->getSyntaxon($ids[1], $depFrFilter = null, $ueFilter = null);
+
+			// Retrieve synonyms according to user's rights
+			$findGoodRepoSynonyms = $this->get('eveg_app.get_synonyms_according_user');
+			$synonyms1 = $findGoodRepoSynonyms->getSynonyms($syntaxon1->getCatminatCode(), $depFrFilter = null, $ueFilter = null);
+			$synonyms2 = $findGoodRepoSynonyms->getSynonyms($syntaxon2->getCatminatCode(), $depFrFilter = null, $ueFilter = null);
+
+			return $this->render('evegAppBundle:Default:compare.html.twig', array(
+				'syntaxon1' => $syntaxon1,
+				'syntaxon2' => $syntaxon2,
+				'synonyms1' => $synonyms1,
+				'synonyms2' => $synonyms2
+			));
+		} else {
+			Throw new HttpException(500, "Can't compare ".$nbItems." item(s).");
+		}
 	}
 }
