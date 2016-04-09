d61251f336a0ae0c7724e14e375033d07007cb8c
steph-del <dev.stephane.delplanque@gmail.com>
2016-04-09 17:17:33 +0200

    typo

--- src/eveg/AppBundle/Entity/SyntaxonPhoto.php
+++ src/eveg/AppBundle/Entity/SyntaxonPhoto.php
@@ -183,7 +183,7 @@ class SyntaxonPhoto
      */
     public function setUser($user)
     {
-        $user->addSyntaconPhoto($this);
+        $user->addSyntaxonPhoto($this);
         $this->user = $user;
 
         return $this;
