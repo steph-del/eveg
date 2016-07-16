6c23710118b7bb8e2837ae71ea11c2a0778302fd
steph-del <dev.stephane.delplanque@gmail.com>
2016-07-16 15:59:22 +0200

    image upload page design + translation

--- src/eveg/AppBundle/Form/Type/SyntaxonPhotoType.php
+++ src/eveg/AppBundle/Form/Type/SyntaxonPhotoType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class SyntaxonPhotoType extends AbstractType
 {
@@ -27,39 +28,70 @@ class SyntaxonPhotoType extends AbstractType
         
         $builder->add('imageFile', 'vich_image', array(
             'required'      => true,
-            'download_link' => true
+            'download_link' => true,
+            'label' => 'eveg.dictionary.file'
         ));
         $builder->add('author', TextType::class, array(
             'attr' => array(
-                'placeholder' => 'Caroline Farvacques'
-            )
+                'placeholder' => 'Louise Michel'
+            ),
+            'label' => 'eveg.dictionary.author'
         ));
         $builder->add('date', DateType::class, array(
-            'widget' => 'single_text'
+            'widget' => 'single_text',
+            'label' => 'eveg.dictionary.date'
         ));
-        $builder->add('country');
-        $builder->add('department');
-        $builder->add('city');
-        $builder->add('locality');
-        $builder->add('title');
-        $builder->add('description');
+        $builder->add('country', TextType::class, array(
+            'label' => 'eveg.dictionary.country'
+            ));
+        $builder->add('department', TextType::class, array(
+            'label' => 'eveg.dictionary.department'
+            ));
+        $builder->add('city', TextType::class, array(
+            'label' => 'eveg.dictionary.town'
+            ));
+        $builder->add('locality', TextType::class, array(
+            'label' => 'eveg.dictionary.locality'
+            ));
+        $builder->add('title', TextType::class, array(
+            'label' => 'eveg.dictionary.title'
+            ));
+        $builder->add('description', TextareaType::class, array(
+            'label' => 'eveg.dictionary.description',
+            'attr' => array(
+                'rows' => 4, 'class' => 'form-control'
+            ),
+        ));
         if($grantedCircle) {
             $builder->add('visibility', ChoiceType::class, array(
-            'choices' => array(
-                'public' => 'Public',
-                'private' => 'Privé',
-                'group' => 'Cercle')
+                'choices' => array(
+                    'public' => 'Public',
+                    'private' => 'Privé',
+                    'group' => 'Cercle'
+                ),
+                'label' => 'eveg.dictionary.visibility',
+                'attr' => array(
+                    'class' => 'form-control',
+                )
             ));
         } else {
             $builder->add('visibility', ChoiceType::class, array(
-            'choices' => array(
-                'public' => 'Public',
-                'private' => 'Privé')
+                'choices' => array(
+                    'public' => 'Public',
+                    'private' => 'Privé'),
+                'label' => 'eveg.dictionary.visibility',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
             ));
         }
         $builder->add('license', ChoiceType::class, array(
             'choices' => array(
-                'CC-BY-CA' => 'CC-BY-CA')
+                'CC-BY-CA' => 'CC-BY-CA'),
+            'label' => 'eveg.dictionary.license',
+            'attr' => array(
+                'class' => 'form-control'
+            )
         ));
 
     }
@@ -68,6 +100,7 @@ class SyntaxonPhotoType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => 'eveg\AppBundle\Entity\SyntaxonPhoto',
+            'choice_translation_domain' => true,
         ));
     }
 
