a8915c074fb81cd7e257c3d36aebebefaa55a3a0
steph-del <dev.stephane.delplanque@gmail.com>
2016-10-07 12:45:24 +0200

    fix issue #5

--- src/eveg/AppBundle/Controller/CompareController.php
+++ src/eveg/AppBundle/Controller/CompareController.php
@@ -5,6 +5,7 @@ namespace eveg\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
