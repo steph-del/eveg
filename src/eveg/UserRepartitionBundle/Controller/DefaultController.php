b10bb6be5e0f1e5e70b60d4e0f2011fa338a6925
steph-del <dev.stephane.delplanque@gmail.com>
2016-10-28 22:09:49 +0200

    addRepartition base controller+route+view

--- src/eveg/UserRepartitionBundle/Controller/DefaultController.php
+++ src/eveg/UserRepartitionBundle/Controller/DefaultController.php
@@ -3,11 +3,30 @@
 namespace eveg\UserRepartitionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use eveg\AppBundle\Entity\SyntaxonCore;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	/**
+	* @ParamConverter("syntaxon_id", class="evegAppBundle:syntaxonCore")
+	* @Security("has_role('ROLE_USER')") 
+	*/
+    public function addRepartitionAction(SyntaxonCore $syntaxon, $id, Request $request)
     {
-        return $this->render('evegUserRepartitionBundle:Default:index.html.twig', array('name' => $name));
+
+
+    	// baseveg repartition maps to Json
+		$serializer = $this->container->get('jms_serializer');
+		$repDepFrJson = $serializer->serialize($syntaxon->getRepartitionDepFr(), 'json');
+		$repUeJson = $serializer->serialize($syntaxon->getRepartitionEurope(), 'json');
+
+        return $this->render('evegUserRepartitionBundle:Default:addRepartition.html.twig',
+        	array('syntaxon'     => $syntaxon,
+        		  'repDepFrJson' => $repDepFrJson,
+        		  'repUeJson'    => $repUeJson,
+        	));
     }
 }
