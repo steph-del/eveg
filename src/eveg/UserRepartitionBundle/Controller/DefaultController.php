e6cc90c414f967ad9ace7e95e47b7b2ed7cddceb
steph-del <dev.stephane.delplanque@gmail.com>
2016-10-29 19:26:26 +0200

    update add repartition controller

--- src/eveg/UserRepartitionBundle/Controller/DefaultController.php
+++ src/eveg/UserRepartitionBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use eveg\AppBundle\Entity\SyntaxonCore;
+use eveg\UserRepartitionBundle\Form\Type\RepartitionType;
 
 class DefaultController extends Controller
 {
@@ -17,6 +18,36 @@ class DefaultController extends Controller
     public function addRepartitionAction(SyntaxonCore $syntaxon, $id, Request $request)
     {
 
+    	$request = $this->getRequest();
+
+		// Creates the form...
+    	$form = $this->createForm(new RepartitionType());
+        // ... and then hydrates it
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $repartition = $form->getData();
+            $currentUser = $this->getUser();
+
+            $repartition->setSyntaxonCited($syntaxon->getSyntaxon());
+            $repartition->setSyntaxonCore($syntaxon);
+            $repartition->setMap('depFr');
+            $repartition->setSubmitedBy($currentUser);
+            $repartition->setSubmitedAt(new \DateTime('now'));
+            $repartition->setValidate(false);
+            
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($repartition);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'Une nouvelle données enregistrée.');
+
+            return $this->redirect($this->generateUrl('eveg_show_one', 
+                    array('id' => $syntaxon->getId())
+                ));
+        }
 
     	// baseveg repartition maps to Json
 		$serializer = $this->container->get('jms_serializer');
@@ -25,6 +56,7 @@ class DefaultController extends Controller
 
         return $this->render('evegUserRepartitionBundle:Default:addRepartition.html.twig',
         	array('syntaxon'     => $syntaxon,
+        		  'form'         =>$form->createView(),
         		  'repDepFrJson' => $repDepFrJson,
         		  'repUeJson'    => $repUeJson,
         	));
