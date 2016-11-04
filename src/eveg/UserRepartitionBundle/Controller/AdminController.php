f80cbb783a7398f22815f88ac8b84babec72c39d
steph-del <dev.stephane.delplanque@gmail.com>
2016-11-04 09:50:12 +0100

    validate & unvalidate actions

--- src/eveg/UserRepartitionBundle/Controller/AdminController.php
+++ src/eveg/UserRepartitionBundle/Controller/AdminController.php
@@ -4,7 +4,9 @@ namespace eveg\UserRepartitionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-//use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use eveg\UserRepartitionBundle\Entity\Repartition;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
@@ -33,7 +35,59 @@ class AdminController extends Controller
     	$repartitions = $repartitionRepo->findUnresolvedRepartitions();
 
     	return $this->render('evegUserRepartitionBundle:Admin:unresolvedRepartitions.html.twig', array(
-    		'repartitions' => $repartitions,
-    	));
+            'repartitions' => $repartitions,
+        ));
+    }
+
+    /**
+    * Validate a Repartition data
+    *
+    * @param \Repartition $repartition repartition object (auto converted by ParamConverter annotation)
+    * @param integer      $id          repartition id
+    * @param \Request     $request
+    *
+    * @ParamConverter("repartition_id", class="evegUserRepartitionBundle:Repartition")
+    * @Security("has_role('ROLE_SUPER_ADMIN')") 
+    */
+    public function validateRepartitionAction(Repartition $repartition, $id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        try {
+            $repartition->setValidated(true);
+            $em->flush();
+            $em->clear();
+            $request->getSession()->getFlashBag()->add('success', 'La donnée #'.$repartition->getId().' a été validée.');
+        } catch(Exception $e) {
+            $request->getSession()->getFlashBag()->add('warning', 'Erreur lors de la validation de la donnée #'.$repartition->getId().'.');
+        }
+
+        return $this->redirect($this->generateUrl('eveg_user_repartition_admin_unresolved'));
+    }
+
+    /**
+    * Unvalidate a Repartition data
+    *
+    * @param \Repartition $repartition repartition object (auto converted by ParamConverter annotation)
+    * @param integer      $id          repartition id
+    * @param \Request     $request
+    *
+    * @ParamConverter("repartition_id", class="evegUserRepartitionBundle:Repartition")
+    * @Security("has_role('ROLE_SUPER_ADMIN')") 
+    */
+    public function unvalidateRepartitionAction(Repartition $repartition, $id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        try {
+            $repartition->setValidated(false);
+            $em->flush();
+            $em->clear();
+            $request->getSession()->getFlashBag()->add('success', 'La donnée #'.$repartition->getId().' a été dévalidée.');
+        } catch(Exception $e) {
+            $request->getSession()->getFlashBag()->add('warning', 'Erreur lors de la dévalidation de la donnée #'.$repartition->getId().'.');
+        }
+
+        return $this->redirect($this->generateUrl('eveg_user_repartition_admin_unresolved'));
     }
 }
