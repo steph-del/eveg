9ce273ba9bb93467f2640eb3ed6eba7a9a5700ae
steph-del <dev.stephane.delplanque@gmail.com>
2017-04-20 19:14:57 +0200

    new Controllers : merge repartition data (dep fr by level & dep fr -> ue map)

---
+++ src/eveg/AppBundle/Controller/RepartitionFrMergeEuMapController.php
@@ -0,0 +1,59 @@
+<?
+// src/eveg/AppBundle/Controller/RepartitionFrMergeEuMapController.php
+
+namespace eveg\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use eveg\AppBundle\Entity\syntaxonRepartitionDepFr;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Part of admin backend 
+ */
+class RepartitionFrMergeEuMapController extends Controller
+{
+
+	/**
+ 	 * @Security("has_role('ROLE_SUPER_ADMIN')")
+ 	 */
+	public function defaultAction()
+	{
+		return $this->render('evegAppBundle:Admin:repartitionMergeFrEuMapDefault.html.twig');
+	}
+
+	/**
+ 	 * @Security("has_role('ROLE_SUPER_ADMIN')")
+ 	 */
+	public function mergeDepFrEuMapAction(Request $request)
+	{
+		if($request->isXmlHttpRequest()) {
+
+			$response = new JsonResponse();
+
+			// retrieves ioHelpers service
+			$ioHelpers = $this->get('eveg_app.io_helpers');
+
+			$em 	   = $this->getDoctrine()->getManager();
+	        $SCrepo    = $em->getRepository('evegAppBundle:SyntaxonCore');
+	        $level     = $request->get('level');
+	        $syntaxons = $SCrepo->getSyntaxonsByLevelWithRepartitions($level);
+	        
+	        // Set Fr (country) = 1 if at less on departement = 1 
+	        foreach ($syntaxons as $keySyntaxon => $syntaxon) {
+	      		if(($ioHelpers->isThereAtLessOnePresentData($syntaxon->getRepartitionDepFr())) && ($syntaxon->getRepartitionEurope()->getFrance() != '1')) {
+	      			$syntaxon->getRepartitionEurope()->setFrance('1');
+	      		} 
+	        }
+
+			// Flush
+			$em->flush();
+
+			$response->setData(array('done'  => $syntaxons));
+			return $response;
+
+		}
+	}
+}
