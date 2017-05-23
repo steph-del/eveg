e14816158ff9f609c680d3b612e7e0f46e09dfe8
steph-del <dev.stephane.delplanque@gmail.com>
2017-05-23 22:15:40 +0200

    add getNodes function

--- src/eveg/PsiBundle/Controller/PsiRestController.php
+++ src/eveg/PsiBundle/Controller/PsiRestController.php
@@ -9,19 +9,40 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
-//use JMS\Serializer\SerializationContext;
+use eveg\PsiBundle\Entity\Node;
 
 class PsiRestController extends FOSRestController
 {
+
+	/**
+	 * GET Route annotation
+	 * Get all nodes
+	 * @Get("/nodes")
+	 */
+	public function getNodesAction()
+	{
+		// New view
+		$view = $this->view();
+
+		// Grabbing Entity manager & repository
+		$em		  = $this->getDoctrine()->getManager('psi_db');
+		$nodeRepo = $em->getRepository('evegPsiBundle:Node');
+
+		$nodes    = $nodeRepo->findAll();
+
+		// Return
+		$view->setData($nodes);
+		return $view;
+	}
+
 	/**
 	 * GET Route annotation.
-	 * @Get("/node/{id}")
+	 * @Get("/nodes/{id}")
 	 */
 	public function getNodeIdAction($id)
 	{
 		// New view
 		$view = $this->view();
-		//$view->setSerializationContext(SerializationContext::create()->setGroups(array('API')));
 		
 		$serializer = $this->container->get('jms_serializer');
 
@@ -31,9 +52,7 @@ class PsiRestController extends FOSRestController
 
 		// Find node by id
 		$node 	  = $nodeRepo->findById($id);
-		//$output = $serializer->serialize($node, 'json');
-//dump($node);
-		//return new JsonResponse($output);
+
 		// Return
 		$view->setData($node);
 		return $view;
