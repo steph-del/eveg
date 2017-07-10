951d0b4b93047b2a3a014ddeb2d1a81bbede42ef
steph-del <dev.stephane.delplanque@gmail.com>
2017-07-10 09:51:48 +0200

    Add PUT nodes Controller

--- src/eveg/PsiBundle/Controller/PsiRestController.php
+++ src/eveg/PsiBundle/Controller/PsiRestController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Put;
 use eveg\PsiBundle\Entity\Node;
 
 class PsiRestController extends FOSRestController
@@ -57,5 +58,63 @@ class PsiRestController extends FOSRestController
 		$view->setData($node);
 		return $view;
 
+	}
+
+	/**
+	 * PUT Route annotation.
+	 * @Put("/nodes")
+	 */
+	public function putNodeAction(Request $request)
+	{
+		/*
+		 * About the requested data :
+		 * $request contains serialized data from front app :
+		 * eg : {"frontId":1498505893522,"nodes":[{"frontId":1498505893522,"name":"lolium perenne","coef":"1"},{"frontId":1498505900103,"name":"agrostis stolonifera","coef":"1"}]}
+		 * this example is a simple node (only described by its frontId) with 2 child nodes
+		 * Data from the front app should always have this pattern : {rootNode,"nodes":[...]}
+		 */
+
+		$em		  = $this->getDoctrine()->getManager('psi_db');
+		$nodeRepo = $em->getRepository('evegPsiBundle:Node');
+
+		$jsonData = $request->get('name');
+		$data = json_decode($request->getContent(), true);
+
+		// Create the root node and hydrate it
+		$rootNode = new Node('synusy');
+		$em->persist($rootNode);
+		$rootNode->setRepository('baseveg');
+		$rootNode->setName('A synusy from my Angular app!');
+		$rootNode->setFrontId($data['frontId']);
+
+		// Iterate through nodes ; create children nodes and hydrate them
+		foreach ($data['nodes'] as $key => $node) {
+			${'node'.$key} = new Node('idiotaxon');
+			
+			$em->persist(${'node'.$key});
+			$rootNode->addChild(${'node'.$key});
+
+			${'node'.$key}->setFrontId($node['frontId']);
+			${'node'.$key}->setName($node['name']);
+			${'node'.$key}->setCoef($node['coef']);
+			${'node'.$key}->setRepository('baseflor');
+		}
+
+		// Flush & clear the EM
+		$em->flush();
+		$em->clear();
+
+		// Return array (frontId) => (id)
+		//$response = array();
+		//$response[$rootNode->getFrontId()] = $rootNode->getId();
+		//foreach ($rootNode->getChildren() as $key => $node) {
+		//	$reponse[$node->getFrontId()] = $node->getId();
+		//}
+
+		// New view
+		$view = $this->view();
+		$view->setData($rootNode);
+
+		return $view;
 	}
 }
